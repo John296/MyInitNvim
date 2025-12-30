3826682d29a078b07246b27e13f6e913588c88d8
John263 <jimmy2972@126.com>
2025-12-30 22:13:41 +0800

    improve blink.cmp selection settings

--- lua/settings/blink.lua
+++ lua/settings/blink.lua
@@ -1,11 +1,34 @@
 require("blink.cmp").setup({
-    keymap = { preset = 'super-tab'},
+    -- keymap = { preset = 'super-tab'},
+    keymap = {
+        ['<tab>'] = {
+            'select_next',
+            fallback = 'accept',
+        },
+
+        ['<S-tab>'] = {
+            'select_prev',
+            fallback = 'accept',
+        },
+    },
+
     appearance = {
         nerd_font_variant = "mono",
     },
-    completion = { documentation = { auto_show = false } },
+
+    completion = {
+        documentation = { auto_show = false },
+        list = {
+            selection = {
+                preselect =  false,
+                auto_insert = true,
+            },
+        },
+    },
+
     sources = {
         default = { "lsp", "path", "snippets", "buffer" },
     },
+
     fuzzy = { implementation = "lua" },
 })
