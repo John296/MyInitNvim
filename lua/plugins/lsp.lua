4f333d36fb8cab537d27d30725933ed7262ec753
John263 <jimmy2972@126.com>
2026-08-21 13:54:13 +0800

    add shortcuts for code_action

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,3 +5,13 @@ vim.pack.add({
 vim.lsp.enable('lua_ls')
 vim.lsp.enable('clangd')
 vim.lsp.enable('pylsp')
+
+vim.keymap.set('n', '<leader>la', vim.lsp.buf.code_action, { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>lf', function()
+    vim.lsp.buf.code_action({
+        apply = true,
+        context = {
+            only = { "source.organizeImports", "quickfix" },
+        },
+    })
+end, { noremap = true, silent = true })
